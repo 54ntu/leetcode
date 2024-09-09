c9689435148278d6c637c3b289bb6c46fa0261e6
54ntu <chaudharysantaram8@gmail.com>
2024-09-09 21:58:36 +0545

    new problems are added

---
+++ sum_of_two_numbers.js
@@ -0,0 +1,17 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.    arr= [1,2,3,5]   target = 7  -->>output should be (1,3)
+
+function twoSum(nums, target) {
+  let arr1 = [];
+  //   console.log(nums[1]);
+  if (nums.length > 0) {
+    for (let i=0; i<nums.length;i++){
+        
+    }
+  }
+}
+
+twoSum([1, 2, 3, 4], 3);
